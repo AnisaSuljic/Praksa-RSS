b94e3a0c65be465668bf08d7edb8c4d1b2a83d0f
AnisaSuljic <62303675+AnisaSuljic@users.noreply.github.com>
2021-04-28 00:02:54 +0200

    dizaj edit, modali

--- Angular CLI/routing/src/app/edit-inputs/edit-inputs.component.ts
+++ Angular CLI/routing/src/app/edit-inputs/edit-inputs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-edit-inputs',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-inputs.component.css']
 })
 export class EditInputsComponent implements OnInit {
-
-  constructor() { }
+  closeResult:string='';
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
+  ToSection(id:string){
+    document.getElementById(id)?.scrollIntoView();
+  }
+  /**Modal GetStavke */
+Get(content:any) {
+  this.modalService.open(content,{ariaLabelledBy: 'modal-basic-title',size:'lg'}).result.then((result) => {
+    this.closeResult = `Closed with: ${result}`;
+  }, (reason) => {
+    this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+  });
+}
 
+private getDismissReason(reason: any): string {
+  if (reason === ModalDismissReasons.ESC) {
+    return 'by pressing ESC';
+  } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+    return 'by clicking on a backdrop';
+  } else {
+    return `with: ${reason}`;
+  }
+}
 }
