ae09931d9d2e9a2bfcec6f8b08666d93910aa0be
AnisaSuljic <62303675+AnisaSuljic@users.noreply.github.com>
2021-04-27 17:22:30 +0200

    edit, modali

---
+++ Angular CLI/routing/src/app/edit-inputs/edit-inputs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-inputs',
+  templateUrl: './edit-inputs.component.html',
+  styleUrls: ['./edit-inputs.component.css']
+})
+export class EditInputsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Angular CLI/routing/src/app/inputs/inputs.component.ts
+++ Angular CLI/routing/src/app/inputs/inputs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-inputs',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls:['/inputs.component.css']
 })
 export class InputsComponent implements OnInit {
-
-  constructor() { }
+  closeResult:string='';
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
+/**Modal Add */
+Add(content:any) {
+  this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.closeResult = `Closed with: ${result}`;
+  }, (reason) => {
+    this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+  });
+}
+/**Modal Delete */
 
+Delete(content2:any) {
+  this.modalService.open(content2, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.closeResult = `Closed with: ${result}`;
+  }, (reason) => {
+    this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+  });
+}
+private getDismissReason(reason: any): string {
+  if (reason === ModalDismissReasons.ESC) {
+    return 'by pressing ESC';
+  } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+    return 'by clicking on a backdrop';
+  } else {
+    return `with: ${reason}`;
+  }
+}
 }
