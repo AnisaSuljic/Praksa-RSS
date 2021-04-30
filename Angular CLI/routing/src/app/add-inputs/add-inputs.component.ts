ad7cd2cf58725ad87dbdee425213981102c6f3ea
AnisaSuljic <62303675+AnisaSuljic@users.noreply.github.com>
2021-04-30 09:38:48 +0200

    redirektanje

--- Angular CLI/routing/src/app/add-inputs/add-inputs.component.ts
+++ Angular CLI/routing/src/app/add-inputs/add-inputs.component.ts
@@ -12,15 +12,24 @@ import { RacunService } from '../services/racun.service';
 })
 export class AddInputsComponent implements OnInit {
   racuni: IRacun;
+
   constructor(private _racunService: RacunService,private router: Router) {
      this.racuni = new IRacun();
     }
+
   ngOnInit(): void {
   }
+  
   onSubmit(){
-    this._racunService.addRacun(this.racuni).subscribe(data=> this.racuni = data);
-    this.router.navigate(["/adminpanel/inputs"]);
+    return this._racunService.addRacun(this.racuni).subscribe(
+      (result)=>{
+        this.router.navigate(["/adminpanel/inputs"]);
+      }
+    );
+  }
 
+  Zatvori(){
+    this.router.navigate(["/adminpanel/inputs"]);
   }
   
 }
